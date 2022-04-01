b1ad63c1b697226b7761b886f1caaf1d7b51ec66
KELES <mehmetkeles@northwesternmutual.com>
2022-04-01 16:42:24 -0400

    exercise 1

---
+++ exercise_1.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+
+acronyms = {}
+
+acronyms['LOL'] = 'Laugh out loud'
+acronyms['IDK'] = "I don't know"
+acronyms['TBH']  = 'To be honest'
+
+
+print("Here is the list of acronyms:" , acronyms)
